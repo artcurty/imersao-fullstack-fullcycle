97424913ca9700a6a4508b1f5329139457fb21f1
artcurty <artcurty@gmail.com>
2021-02-07 12:47:34 -0300

    include domains

---
+++ codePix/domain/model/user.go
@@ -0,0 +1,36 @@
+package model
+
+type User struct {
+	Base `valid:"required"`
+	Name  string `json:"name" gorm:"type:varchar(255)" valid:"notnull"`
+	Email string `json:"email" gorm:"type:varchar(255)" valid:"notnull"`
+}
+
+func (user *User) isValid() error {
+	_, err := govalidator.ValidateStruct(user)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func NewUser(name string, email string) (*User, error) {
+
+	user := User{
+		Name: name,
+		Email: email,
+	}
+
+	user.ID = uuid.NewV4().String()
+	user.CreatedAt = time.Now()
+
+	err := user.isValid()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
---
+++ codePix/go.mod
@@ -0,0 +1,3 @@
+module github.com/artcurty/imersao-fullstack-fullcycle/codePix
+
+go 1.15
